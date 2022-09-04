e4c5a6e2613982f7aa763118a5bc0b598e446644
Harris Bisset <RedstoneRebel399@gmail.com>
2022-09-04 13:12:47 +0100

    Update sync.py

--- sync.py
+++ sync.py
@@ -1,13 +1,14 @@
 import pyautogui
 import time
-#from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw
 import psutil
 #import subprocess
-#import sys
+import sys
 import os
 import pygetwindow as gw
 import urllib.request
-
+import pytesseract
+import cv2
 
 def main():
 
@@ -26,28 +27,75 @@ def main():
         #checks if signed, if not, signs in
         logIN(mainNavWin)
 
+        #add function to wait
+
         #navigate to settings
-        settingsLocation = pyautogui.locateOnScreen(r'C:\Users\harri\OneDrive\Documents\mtgoAddon\images\settings.png', confidence=.70, region = (2435, 38, 52, 60))
+        settingsLocation = pyautogui.locateOnScreen(r'.\images\settings.png', confidence=.70, region = (2435, 38, 52, 60))
         pyautogui.click(settingsLocation)
 
         #navigate to game history
-        gameHistLocation = pyautogui.locateOnScreen(r'C:\Users\harri\OneDrive\Documents\mtgoAddon\images\game-history.png', confidence=.70, region = (12, 386, 190, 31))
+        gameHistLocation = pyautogui.locateOnScreen(r'.\images\game-history.png', confidence=.70, region = (0, 377, 292, 46))
         pyautogui.click(gameHistLocation)
+        time.sleep(0.25)
 
-        #click on game history
-        #navigate to top of records
-        #while recordID != None:
-            #select next record
-            #get data
-            #navigate to replays
-            #for replay in replays:
-                #click on replay
-                #if open app:
-                    #record data
-                #else:
-                    #break
+        recordID = 0 #highest recordID from db (local or serverside)
+        top = 368
+        recordLoop = 0
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-            #send data for record to imGui
+        #while True:
+        #for loops in range(0, 3):
+        #takes screenshot of current record, each record is 40 tall
+        pyautogui.screenshot('scrn.png', region=(285, top, 2000, 40))
+
+        #gets data from screenshot and turns it into array
+        imgText_rgb = cv2.imread('scrn.png')
+        imgText_rgb = get_grayscale(imgText_rgb)
+        record = pytesseract.image_to_string(imgText_rgb)
+
+        #converts to better time
+        if record[6] == 'PM':
+            temp = record[5].split(':')
+            temp[0] = int(temp[0]) + 12
+            record[5] = ':'.join(temp)
+            record.remove(6)
+        
+        pyautogui.moveTo(400, top+20)
+        details = pyautogui.locateOnScreen(r'.\images\details.png')
+        pyautogui.click(details)
+        time.sleep(0.5)
+        
+        games = list(pyautogui.locateAllOnScreen(r'.\images\replayGame.png'))
+        #if not games:
+            #break
+        
+        for game in games:
+            pyautogui.click(game)
+            #check if new window open
+            #else skip
+
+            #each scroll moves down 3 records
+            #recordLoop = recordLoop + 1
+            #if recordLoop = 3:
+                #top = 368
+                #pyautogui.scroll(1)
+            #top = top + 40
+            #recordID = recordID + 1
+            #pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         pass
 
 
@@ -65,16 +113,23 @@ def openApplication():
     return 
 
 def logIN(mainNavWin):
+
+    #update to retrive stored user preference data
+
+    #if not username:
+        #send message to display = "Please open and login to MTGO"
+        #exit
+
     #sets autoFilled to false, first, for userName
     autoFilled = False
     #checks if it's on the login screen
-    loginLocation = pyautogui.locateOnScreen(r'C:\Users\harri\OneDrive\Documents\mtgoAddon\images\login.png', confidence=.65, region = (127, 673, 388, 117))
+    loginLocation = pyautogui.locateOnScreen(r'.\images\login.png', confidence=.65, region = (127, 673, 388, 117))
 
     #if true
     if loginLocation:
         try:
-            password = pyautogui.locateOnScreen(r'C:\Users\harri\OneDrive\Documents\mtgoAddon\images\password.png', confidence=.70, region = (122, 609, 399, 55))
-            screenName = pyautogui.locateOnScreen(r'C:\Users\harri\OneDrive\Documents\mtgoAddon\images\screenName.png', confidence=.70, region = (122, 556, 400, 54))
+            password = pyautogui.locateOnScreen(r'.\images\password.png', confidence=.70, region = (122, 609, 399, 55))
+            screenName = pyautogui.locateOnScreen(r'.\images\screenName.png', confidence=.70, region = (122, 556, 400, 54))
             uN = input("Username: ")
         except:
             #if program can't detect screenName, but login detected, screenName is autoFilled
@@ -102,15 +157,35 @@ def logIN(mainNavWin):
 
 def userNamePass(button, inputData):
     pyautogui.click(button)
-    pyautogui.press('backspace', presses=25)
+    pyautogui.press('backspace', presses=26)
     pyautogui.write(inputData)
 
 
 def checkConnection(host='http://google.com'):
+
+    #add bit here to check for update to program
+    #compare version in package.json
+    
     try:
         urllib.request.urlopen(host)
         return True
     except:
         return False
+
+
+def ocr_core(img):
+    text = pytesseract.image_to_string(img)
+    return text
+
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
 
 main()
