118d39714f995742996da39f13aa162b45c8c65c
Harris Bisset <RedstoneRebel399@gmail.com>
2022-09-16 18:18:32 +0100

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,7 +1,8 @@
+
 function runPythonSync(){
     var pyshell =  require('python-shell');
 
-    pyshell.PythonShell.run('sync.py', null, function  (err, results)  {
+    pyshell.PythonShell.run('./python/sync.py', null, function  (err, results)  {
       if  (err)  throw err;
       console.log('sync.py finished.');
     });
@@ -9,29 +10,33 @@ function runPythonSync(){
 
 function runPythonDB(){
     var pyshell =  require('python-shell');
-
     var pjson = require('./package.json');
-
 
     let opDataLoad = {
       args: ["loaded", pjson.version]
     }
 
-    pyshell.PythonShell.run('dbCMD.py', opDataLoad, function  (err, results)  {
+    pyshell.PythonShell.run('./python/dbCMD.py', opDataLoad, function  (err, results)  {
+      if (results == "createdDB"){
+        const { BrowserWindow } = require('electron')
+        const winInput = new BrowserWindow({ show: false })
+        winInput.loadFile('./forms/inputUserData.html')
+        winInput.once('ready-to-show', () => {
+          winInput.show()
+        })
+      }
+
       if  (err)  throw err;
-      console.log('dbCMD.py finished.');
+        console.log('dbCMD.py finished.');
     });
 }
 
 document.querySelector('#pyBtnSync').addEventListener('click', () => {
-    
-    //
-    document.getElementById('#pyBtnSync').style.display = "none"
-    
     //run pythonfile that grabs user preference data from db
     //runPython(autoLogin = T/F, userName = '', password = '', )
     runPythonSync()
-    
-    //
-    document.getElementById('#pyBtnSync').style.display = "block"
+})
+
+document.addEventListener('DOMContentLoaded', () =>{
+    runPythonDB()
 })
