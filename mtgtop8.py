dee7d553244b5ba088fc688c65064537d8e678e6
Harris Bisset <RedstoneRebel399@gmail.com>
2022-09-16 11:01:35 +0100

    Update mtgtop8.py

--- mtgtop8.py
+++ mtgtop8.py
@@ -1,4 +1,5 @@
 from ast import Return
+from tkinter import E
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,6 +9,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import (ElementNotVisibleException,ElementNotSelectableException)
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
 
 gameNo = 1
 date = "04/09/2022"
@@ -32,17 +35,17 @@ class DriverController():
         select_element = self.driver.find_element(By.CSS_SELECTOR, 'select[name="format"]')
         select_object = Select(select_element)
         select_object.select_by_visible_text(format)
-        cont = True
 
         #click cookie banner
         self.driver.find_element(By.XPATH, '//*[@id="cookie_window"]/div[2]/button').click()
 
-        if gameNo == 1:
-            SB = ('',)
-    
+        if gameNo > 1:
+            cards = MB
+        else:
+            cards = MB + SB
+
         self.driver.find_element(By.XPATH, '//input[@name="SB_check"]').click()
         textarea = self.driver.find_element(By.XPATH, '//textarea[@name="cards"]')
-        cards = MB + SB
         for listCard in range(0, len(cards)-1):
             textarea.send_keys(cards[listCard])
             textarea.send_keys(Keys.RETURN)
@@ -57,15 +60,47 @@ class DriverController():
 
 
     def getRecord(self, currentDeck, url):
+        print('getting record')
+
         self.driver.get(url)
-        WebDriverWait(self.driver, timeout=20, poll_frequency=5, ignored_exceptions=[ElementNotVisibleException,ElementNotSelectableException]).until(EC.element_to_be_clickable((By.XPATH, "//html/body/div/div/table/tbody/tr/td[2]/form/table/tbody/tr[2]/td[2]/a")), 'Timed out waiting for element')
+        print(url)#ignored_exceptions=[ElementNotVisibleException,ElementNotSelectableException]
+
+        WebDriverWait(self.driver, timeout=5).until(lambda d: d.find_element(By.XPATH,f"//body/div/div/table/tbody/tr/td[2]/form/table/tbody/tr[{currentDeck}]/td[2]/a"))
+
         deckLink = self.driver.find_element(By.XPATH, f"//body/div/div/table/tbody/tr/td[2]/form/table/tbody/tr[{currentDeck}]/td[2]/a")
+        # try:
+        #     wait = WebDriverWait(self.driver,3)
+        #     element = wait.until(EC.presence_of_element_located((By.XPATH, '//html/body/div/div/div[2]/div[1]/div[1]/a/img')))
+        # except Exception as nse:
+        #     print(nse)
+        #     print("-----")
+        #     print(str(nse))
+        #     print("-----")
+        #     print(nse.args)
+        #     print("=====")
+        #     self.driver.quit()
+        # else:
+        #     pass
+        # finally:
+        #     pass
+
         deckName = deckLink.text
 
         #update to driver.get
-        deckLink.click()
+        urlDeck = deckLink.get_attribute('href')
+        print(urlDeck)
+        self.driver.get(urlDeck)
 
-        WebDriverWait(self.driver, timeout=20, poll_frequency=5, ignored_exceptions=[ElementNotVisibleException,ElementNotSelectableException]).until(EC.element_to_be_clickable((By.XPATH, "//html/body/div/div/div[2]/div[1]/div[1]/a")), 'Timed out waiting for element')
+        #WebDriverWait(self.driver, timeout=7, poll_frequency=5, ignored_exceptions=[ElementNotVisibleException,ElementNotSelectableException]).until(EC.element_to_be_clickable((By.XPATH, "//html/body/div/div/div[2]/div[1]/div[1]/a")), 'Timed out waiting for element')
+        try:
+            WebDriverWait(self.driver, timeout=3, poll_frequency=5, ignored_exceptions=[ElementNotVisibleException]).until(EC.element_to_be_clickable((By.XPATH, '//html/body/div/div/div[2]/div[1]/div[1]/a/img')), 'Timed out waiting for element')
+        except NoSuchElementException as nse:
+            print(nse)
+            print("-----")
+            print(str(nse))
+            print("-----")
+            print(nse.args)
+            print("=====")
         #deck = currentDeck.find_elements(By.XPATH, '//td[@class="S12"]')
         #deckName = deck.getText()
         decklist = []
