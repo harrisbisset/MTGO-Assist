ab9c2803291631b2bd2470b98cbdbdae868b60f5
Harris Bisset <RedstoneRebel399@gmail.com>
2022-09-17 20:43:30 +0100

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,4 @@
+window.$ = window.jQuery = require('jquery');
 
 function runPythonSync(){
     var pyshell =  require('python-shell');
@@ -5,6 +6,21 @@ function runPythonSync(){
     pyshell.PythonShell.run('./python/sync.py', null, function  (err, results)  {
       if  (err)  throw err;
       console.log('sync.py finished.');
+    });
+}
+
+function runPythonCreateUser(userName, userPass, mtgoName, mtgoPass){
+  var pyshell =  require('python-shell');
+  
+  let opDataLoad = {
+    mode: 'text',
+    pythonOptions: ['-u'],
+    args: ['createUser', userName, userPass, mtgoName, mtgoPass]
+  }
+  pyshell.PythonShell.run('./python/dbCMD.py', opDataLoad, function  (err, results)  {
+      if  (err)  throw err;
+      console.log('dbCMD.py finished.');
+      console.log('results: ', results);
     });
 }
 
@@ -20,18 +36,55 @@ function runPythonDB(){
 
     pyshell.PythonShell.run('./python/dbCMD.py', opDataLoad, function  (err, results)  {
 
-      //switch back to createdDB
-      if (results[0] == "connectedDB"){
-        const { BrowserWindow } = require('electron')
-        const winInput = new BrowserWindow({ show: false })
-        winInput.loadFile('./forms/inputUserData.html')
-        winInput.once('ready-to-show', () => {
-          winInput.show()
-        })
-      }
-      if  (err)  throw err;
+      if (results[0] === "unconnectedDB"){
+        const Dialogs = require('dialogs')
+        const dialogs = Dialogs()
+        dialogs.confirm('Please create account for MTGO Assist', ok => {
+          console.log('result:', ok);
+          if (ok !== undefined) {
+            dialogs.prompt('Username', ok => {
+              console.log('result:', ok);
+              var userName = ok
+              if (ok !== undefined) {
+                dialogs.promptPassword('Password', ok => {
+                  console.log('result:', ok);
+                  var userPass = ok
+                  if (ok !== undefined) {
+                  dialogs.alert('Account Created', ok =>{
+                      dialogs.confirm('Would you like to link your MTGO account?', ok => {
+                        console.log('result:', ok);
+                        if (ok !== undefined) {
+                          dialogs.prompt('Username', ok => {
+                            console.log('result:', ok);
+                            var mtgoName = ok
+                            if (ok !== undefined) {
+                              dialogs.promptPassword('Password', ok => {
+                                console.log('result:', ok);
+                                var mtgoPass = ok
+                                dialogs.alert('Account Linked', ok =>{
+
+                                  //sends data to db
+                                  runPythonCreateUser(userName, userPass, mtgoName, mtgoPass);
+                                });
+                              });
+                            };
+                          });
+                        }else if(ok === undefined){
+                          runPythonCreateUser(userName, userPass, "", "");
+                        }
+                      });
+                    });
+                  };
+                });
+              };
+            });
+          };
+        });
+      };
+      
+      if(err)  throw err;
       console.log('dbCMD.py finished.');
-      console.log('results: %j', results);
+      console.log('results: ', results);
     });
 }
 
