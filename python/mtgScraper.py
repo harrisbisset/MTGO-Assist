4bd520cf76d1ee6960dea9325d5d6d43df4ce127
Harris Bisset <RedstoneRebel399@gmail.com>
2023-01-05 13:31:38 +0000

    Update mtgScraper.py

--- python/mtgScraper.py
+++ python/mtgScraper.py
@@ -6,15 +6,22 @@ from datetime import datetime
 def main(path, player):
     fileList = [f for f in os.listdir(path) if f.endswith('.dat')]
 
+    #opens connection
+    openConn()
+    
+    #gets
+    
     #initilises modules
     match = MatchRecord(player)
     dc = DriverController()
+    
 
     #loops through file list
     for filename in fileList:
 
         #gets decklists from MatchRecord
-        decklists, matchLog, gameRecord, onPlay, startingHands = match.getDecklists(f'{path}/{filename}')
+        #other stored data is implemented into database in MatchRecord
+        decklists = match.getDecklists(f'{path}/{filename}')
 
         #if the file is valid
         if decklists is not None:
@@ -27,14 +34,25 @@ def main(path, player):
             dictNames = dc.returnDictNames(None, decklists, date)
 
             #sends info to sqliite db
-            sqllite(decklists, date, dictNames)
+            sqlliteDriverData(date, dictNames)
 
     #closes webdriver
     dc.quit()
     
-def sqllite(decklists, date, dictNames, matchLog, gameRecord, onPlay, startingHands):
+    #close connection
+    closeConn()
+
+def sqlliteGetID():
+    pass
+    
+def sqlliteDriverData(date, dictNames):
     pass
 
+def openConn():
+    pass
+    
+def closeConn():
+    pass
 
 if __name__ == '__main__':
     main('C:\Users\harri\AppData\Local\Apps\2.0\Data\JWMNX0QY.YK3\AGMD182G.AAW\mtgo..tion_92a8f782d852ef89_0003.0004_4d4c5524cb8c51a2\Data\AppFiles\E8BC386C00E942D40363482907EEDEEA', None)
