050889bfc4cb019bafacc2894998dbc6cc84f4ec
Harris Bisset <RedstoneRebel399@gmail.com>
2023-01-23 10:38:51 +0000

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
+    
+    
+    
+    //i have no idea what this does
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
       if (element) element.innerText = text
