66ea41c4e294ce2821ed5d29cc7f5070638e5109
Harris Bisset <RedstoneRebel399@gmail.com>
2023-01-31 21:27:23 +0000

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,75 +1,65 @@
 //not sure if needed
-window.$ = window.jQuery = require('jquery');
+// window.$ = window.jQuery = require('jquery');
 
 function runPythonSync(){
     var pyshell =  require('python-shell');
-    var highestRecordID
 
-    let syncDataLoad = {
-      mode: 'text',
-      pythonOptions: ['-u'],
-      args: [highestRecordID]
-    }
-
-    pyshell.PythonShell.run('./python/sync.py', syncDataLoad, function  (err, results)  {
-
-      //if 'unconnectedInt returned'
-      if (results[0] === "unconnected") {
-        
-      } else if (results[0] === "license") {
-        const Dialogs = require('dialogs')
-        const dialogs = Dialogs()
-        dialogs.alert('Please close the end user license agreement!', ok =>{})
-      } else if (results[0] === "unOpened") {
-        console.log(results[0])
-        const Dialogs = require('dialogs')
-        const dialogs = Dialogs()
-        dialogs.alert('Please open the program!', ok =>{})
-      }
+    pyshell.PythonShell.run('./python/sync.py', function  (err, results)  {
       if  (err)  throw err;
       console.log('sync.py finished.');
     });
 }
 
-function runPythonDB(){
+function loadNextPage(){
     var pyshell =  require('python-shell');
     var pjson = require('./package.json');
 
     let cmdDataLoad = {
       mode: 'text',
       pythonOptions: ['-u'],
-      args: ["loaded", pjson.version]
+      args: ["nextPage", pjson.version]
     }
 
     pyshell.PythonShell.run('./python/dbCMD.py', cmdDataLoad, function  (err, results)  {
-      
-      if (results[2] === "unconnectedInt"){
-        unInt();
-      }else if (results[2] === "connectedInt"){
-        //hides reload button
-        document.getElementById('#btnReload').style.display = "none";
-          
-        //should also show sync button (set it to hidden first)
-      }
+
+      //append to table
 
       if(err)  throw err;
-      console.log('dbCMD.py finished.');
-      console.log('results: ', results);
+      console.log('loaded next page');
     });
 }
 
-document.querySelector('#pyBtnSync').addEventListener('click', () => {
-    //runs dbCMD that grabs user preference data from db
-    //runPython(autoLogin = T/F, userName = '', password = '', )
+function loadFirstPage(){
+  var pyshell =  require('python-shell');
+  var pjson = require('./package.json');
 
-    //runs main program
+  let cmdDataLoad = {
+    mode: 'text',
+    pythonOptions: ['-u'],
+    args: ["loadFirstPage", pjson.version]
+  }
+
+  pyshell.PythonShell.run('./python/dbCMD.py', cmdDataLoad, function  (err, results)  {
+
+    //append to table
+
+    if(err)  throw err;
+    console.log('loaded next page');
+  });
+}
+
+document.querySelector('#pyBtnSync').addEventListener('click', () => {
     runPythonSync();
 })
 
-document.querySelector('#btnReload').addEventListener('click', () => {
-  runPythonDB();
+document.querySelector('#pyBtnNextPage').addEventListener('click', () => {
+  //runs dbCMD that grabs user preference data from db
+  //runPython(autoLogin = T/F, userName = '', password = '', )
+
+  //runs main program
+  loadNextPage();
 })
 
 document.addEventListener('DOMContentLoaded', () =>{
-  runPythonDB();
+  loadFirstPage();
 })
