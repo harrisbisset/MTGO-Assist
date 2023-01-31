b032cf09d6f3a760f737f793c9dd90655369c49e
Harris Bisset <RedstoneRebel399@gmail.com>
2023-01-31 21:25:37 +0000

    Create dbCMD.py

---
+++ python/dbCMD.py
@@ -0,0 +1,32 @@
+import sqlite3
+import sys
+
+
+#gets data from database to be displayed on table
+def openData():
+    userConnection = sqlite3.connect("./database/mtgoAssist.db")
+    cursor = userConnection.cursor()
+
+
+
+    cursor.close()
+    userConnection.close()
+
+
+#gets user averages
+def getUserData():
+    userConnection = sqlite3.connect("./database/mtgoAssist.db")
+    cursor = userConnection.cursor()
+    
+
+    cursor.close()
+    userConnection.close()
+
+
+if __name__ == "main":
+    pass
+else:
+    if sys.argv[1] == 'loaded':
+        openData()
+    if sys.argv[1] == 'getProfileData':
+        getUserData()
