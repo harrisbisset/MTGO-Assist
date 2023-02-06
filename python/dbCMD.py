cde98741d7e6d17f844f981a95fa30d73cdc9ae5
Harris Bisset <RedstoneRebel399@gmail.com>
2023-02-06 10:27:49 +0000

    Update dbCMD.py

--- python/dbCMD.py
+++ python/dbCMD.py
@@ -7,20 +7,26 @@ def openData():
     userConnection = sqlite3.connect("./database/mtgoAssist.db")
     cursor = userConnection.cursor()
 
-
+    matches = cursor.execute("SELECT * FROM matches ORDER BY matchID DESC limit 20;").fetchall()
 
     cursor.close()
     userConnection.close()
 
+    for row in matches:
+        for i, elem in enumerate(row):
+            if i
+            print(elem)
+
 
 #gets user averages
 def getUserData():
-    userConnection = sqlite3.connect("./database/mtgoAssist.db")
+    userConnection = sqlite3.connect("../database/mtgoAssist.db")
     cursor = userConnection.cursor()
     
 
     cursor.close()
     userConnection.close()
+
 
 
 if __name__ == "main":
