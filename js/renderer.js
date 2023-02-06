27c7af30241d9eaf5e0bd3c0fffb888170497ff6
Harris Bisset <RedstoneRebel399@gmail.com>
2023-02-06 10:32:26 +0000

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,6 +1,3 @@
-//not sure if needed
-// window.$ = window.jQuery = require('jquery');
-
 function runPythonSync(){
     var pyshell =  require('python-shell');
 
@@ -36,12 +33,43 @@ function loadFirstPage(){
   let cmdDataLoad = {
     mode: 'text',
     pythonOptions: ['-u'],
-    args: ["loadFirstPage", pjson.version]
+    args: ["loaded", pjson.version]
   }
 
   pyshell.PythonShell.run('./python/dbCMD.py', cmdDataLoad, function  (err, results)  {
-
-    //append to table
+    
+    let matchID = [];
+    let filename = [];
+    let players = [];
+    let decknames = [];
+    let p1Deck = [];
+    let p2Deck = [];
+    let turnOrder = [];
+    let winList = [];
+    let format = [];
+    let type = [];
+    let date = [];
+    
+    for (let inner = 1; inner < results.length; inner++){
+      
+    }
+    
+    for (let outer = 0; outer < results.length/11; outer++){
+      document.getElementById(databaseContent).innerHTML = `
+          <tr>
+            <th class="matchID">${matchID}</th>
+            <th class="filename">${filename}<button class="replaceImageFilename">...</button></th>
+            <th class="players"><p class="replaceTextOpponent">${players}</p><button class="replaceImageOpponent">...</button></th>
+            <th class="decknames"><p class="replaceText">${decknames}</p><button class="replaceImage">...</button></th>
+            <th class="p1Deck"><p class="replaceTextDeck">${p1Deck}</p><button class="replaceImageDeck">...</button></th>
+            <th class="p2Deck"><p class="replaceTextDeck">${p2Deck}</p><button class="replaceImageDeck">...</button></th>
+            <th class="turnOrder"><p class="replaceTextExtra">${turnOrder}<button class="replaceImageExtra">...</button></th>
+            <th class="winList"><p class="replaceTextExtra">${winList}</p><button class="replaceImageExtra">...</button></th>
+            <th class="format">${format}</th>
+            <th class="type">${type}</th>
+            <th class="date">${date}</th>
+          </tr>`
+    }
 
     if(err)  throw err;
     console.log('loaded next page');
