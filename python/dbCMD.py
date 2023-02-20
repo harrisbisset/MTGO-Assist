d87ecacd9d08c8c598530857bc081d8a8dc1fb6f
Harris Bisset <RedstoneRebel399@gmail.com>
2023-02-20 17:07:59 +0000

    Update dbCMD.py

--- python/dbCMD.py
+++ python/dbCMD.py
@@ -6,22 +6,24 @@ import json
 def openData():
     userConnection = sqlite3.connect("./database/mtgoAssist.db")
     cursor = userConnection.cursor()
+    try:
+        matches = cursor.execute("SELECT * FROM matches ORDER BY matchID DESC limit 20;").fetchall()
+        
+        results = {}
 
-    matches = cursor.execute("SELECT * FROM matches ORDER BY matchID DESC limit 20;").fetchall()
+        for matchTuple in matches:
+            results[matchTuple[0]] = [{}]
+            for data in matchTuple:
+                if isinstance(data, int) == False:
+                    data = json.loads(data)
+                    results[matchTuple[0]][0][list(data.keys())[0]] = data[list(data.keys())[0]]
 
-    cursor.close()
-    userConnection.close()
-    
-    results = {}
-
-    for matchTuple in matches:
-        results[matchTuple[0]] = [{}]
-        for data in matchTuple:
-            if isinstance(data, int) == False:
-                data = json.loads(data)
-                results[matchTuple[0]][0][list(data.keys())[0]] = data[list(data.keys())[0]]
-                
-    print(json.dumps(results))
+        print(json.dumps(results))
+    except:
+        pass
+    finally:
+        cursor.close()
+        userConnection.close()
 
 
 
@@ -29,10 +31,10 @@ def openData():
 def getUserData():
     userConnection = sqlite3.connect("../database/mtgoAssist.db")
     cursor = userConnection.cursor()
-    
 
     cursor.close()
     userConnection.close()
+
 
 if __name__ == "main":
     pass
