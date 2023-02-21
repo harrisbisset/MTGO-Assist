4728fb4af4387cd55892c064500006052a7446eb
Harris Bisset <RedstoneRebel399@gmail.com>
2023-02-21 18:32:40 +0000

    Update dbCMD.py

--- python/dbCMD.py
+++ python/dbCMD.py
@@ -1,7 +1,6 @@
 import sqlite3
 import sys
 import json
-import ast
 
 #gets data from database to be displayed on table
 def openData():
@@ -28,71 +27,74 @@ def openData():
 
 
 
+
 #gets user averages
 def getUserData():
-    userConnection = sqlite3.connect("./database/mtgoAssist.db")
-    cursor = userConnection.cursor()
-    
-    import sqlite3
-import sys
-import json
-import ast
 
-#gets data from database to be displayed on table
-def openData():
-    userConnection = sqlite3.connect("./database/mtgoAssist.db")
-    cursor = userConnection.cursor()
     try:
-        matches = cursor.execute("SELECT * FROM matches ORDER BY matchID DESC limit 20;").fetchall()
-        
-        results = {}
-
-        for matchTuple in matches:
-            results[matchTuple[0]] = [{}]
-            for data in matchTuple:
-                if isinstance(data, int) == False:
-                    data = json.loads(data)
-                    results[matchTuple[0]][0][list(data.keys())[0]] = data[list(data.keys())[0]]
-
-        print(json.dumps(results))
+        with open('python\playerName.txt', 'rb') as f:
+            player = f.read().decode(encoding='utf-8', errors='replace')
     except:
-        pass
-    finally:
-        cursor.close()
-        userConnection.close()
+        print('NA')
+        return
 
-
-
-#gets user averages
-def getUserData():
     userConnection = sqlite3.connect("./database/mtgoAssist.db")
     cursor = userConnection.cursor()
 
     matches = cursor.execute("SELECT winner FROM matches;").fetchall()
-    print(matches)
+
     results = {}
-
     for num, match in enumerate(matches):
-        results[num] = match[0]
-        print(match[0])
+        results[num] = json.loads(match[0])
+    
+    winList = []
+    count = 0
 
-        # print(json.dumps(results)
-    print(results)
-    print(type(results ))
+    for match in results:
+        for res in results[match]['winner']:
+            if res != 'NA':
+                print(res)
+                if res == player:
+                    count += 1
+                winList.append(res)
+
+    print(count / len(winList))
 
     cursor.close()
     userConnection.close()
 
+
+
+
 def getOppWinrate(opponent):
+    
+    try:
+        with open('python\playerName.txt', 'rb') as f:
+            player = f.read().decode(encoding='utf-8', errors='replace')
+    except:
+        print('NA')
+        return
+
     userConnection = sqlite3.connect("./database/mtgoAssist.db")
     cursor = userConnection.cursor()
 
-    matches = cursor.execute("SELECT opponent, winner FROM matches;").fetchall()
-    
-    #[[opponent, [name, name...]],[]]
-    results = matches
+    matches = cursor.execute("SELECT opponent, winner FROM matches ORDER BY opponent;").fetchall()
 
-    record = {0:[]}
+
+    #[[opponent, [name, name...]],[]]
+    results = []
+    for num, match in enumerate(matches):
+        results.append([])
+        for nom, item in enumerate(match):
+            if nom == 0:
+                results[num].append(json.loads(item)['players'][0])
+
+            else:
+                results[num].append(json.loads(item)['winner'])
+            #results[num] = match[0]
+
+
+    record = []
 
     while True:
         low = 0
@@ -114,66 +116,30 @@ def getOppWinrate(opponent):
             # means x is present at mid
             else:
                 for win in results[mid][1]:
-                    record[0].append(win)
+                    record.append(win)
+                results.pop(mid)
     
         # If we reach here, then the element was not present
         break
 
-    print(record)
+    count = 0
+    for play in record:
+        print(play)
+        if play == player:
+            count += 1
+
+    print(count / len(record))
+
+
+if __name__ == "main":
+    pass
+else:
+    if sys.argv[1] == 'loaded':
+        openData()
+    elif sys.argv[1] == 'profile':
+        getUserData()
+    elif sys.argv[1] =='opponent':
+        getOppWinrate(sys.argv[2])
     
 
 
-
-# if __name__ == "main":
-#     pass
-# else:
-#     if sys.argv[1] == 'loaded':
-#         openData()
-#     elif sys.argv[1] == 'profile':
-#         getUserData()
-#     elif sys.argv[1] =='opponent':
-#         getOppWinrate(sys.argv[2])
-
-
-
-
-test = [('{"winner": ["Corzalan", "Corzalan"]}',), ('{"winner": ["sc00nes", "-Sire", "sc00nes"]}',), ('{"winner": ["-Sire", "cbouz", "NA"]}',), ('{"winner": ["lindoso01", "lindoso01"]}',), ('{"winner": ["-Sire", "callowaysmith1", "-Sire"]}',), ('{"winner": ["Cesta", "Cesta"]}',), ('{"winner": ["Brivenix", "NA", "NA"]}',), ('{"winner": ["Magic4ever_MTGO", "Magic4ever_MTGO"]}',), ('{"winner": ["Pacto_das_Guildas", "Pacto_das_Guildas"]}',), ('{"winner": ["-Sire", "NA", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["CaligulaFanBoi", "CaligulaFanBoi"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Foresterf", "Foresterf"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["NA", "NA", "PauperFeito"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "bergamo83", "-Sire"]}',), ('{"winner": ["ResponsiblyStupid", "ResponsiblyStupid"]}',), ('{"winner": ["lindoso01"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Beicodegeia", "Beicodegeia"]}',), ('{"winner": ["Arsmith8", "-Sire", "Arsmith8"]}',), ('{"winner": ["Corzalan", "Corzalan"]}',), ('{"winner": ["sc00nes", "-Sire", "sc00nes"]}',), ('{"winner": ["-Sire", "cbouz", "NA"]}',), ('{"winner": ["lindoso01", "lindoso01"]}',), ('{"winner": ["-Sire", "callowaysmith1", "-Sire"]}',), ('{"winner": ["Cesta", "Cesta"]}',), ('{"winner": ["Brivenix", "NA", "NA"]}',), ('{"winner": ["Magic4ever_MTGO", "Magic4ever_MTGO"]}',), ('{"winner": ["Pacto_das_Guildas", "Pacto_das_Guildas"]}',), ('{"winner": ["-Sire", "NA", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["CaligulaFanBoi", "CaligulaFanBoi"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Foresterf", "Foresterf"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["NA", "NA", "PauperFeito"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "bergamo83", "-Sire"]}',), ('{"winner": ["ResponsiblyStupid", "ResponsiblyStupid"]}',), ('{"winner": ["lindoso01"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Beicodegeia", "Beicodegeia"]}',), ('{"winner": ["Arsmith8", "-Sire", "Arsmith8"]}',), ('{"winner": ["Hoarse_Duck", "Hoarse_Duck"]}',)]
-
-def getUserDataTemp(test):
-    results = {}
-
-    for num, match in enumerate(test):
-        results[num] = json.loads(match[0])
-        print(match[0])
-
-        # print(json.dumps(results)
-
-getUserDataTemp(test)
-
-    matches = cursor.execute("SELECT winner FROM matches;").fetchall()
-    print(matches)
-    results = {}
-
-    for num, match in enumerate(matches):
-        results[num] = match[0]
-        print(match[0])
-
-        # print(json.dumps(results)
-    print(results)
-    print(type(results ))
-
-    cursor.close()
-    userConnection.close()
-
-print(getUserData())
-
-# if __name__ == "main":
-#     pass
-# else:
-#     if sys.argv[1] == 'loaded':
-#         openData()
-#     if sys.argv[1] == 'profile':
-#         getUserData()
-
-
-test = """[('{"winner": ["Corzalan", "Corzalan"]}',), ('{"winner": ["sc00nes", "-Sire", "sc00nes"]}',), ('{"winner": ["-Sire", "cbouz", "NA"]}',), ('{"winner": ["lindoso01", "lindoso01"]}',), ('{"winner": ["-Sire", "callowaysmith1", "-Sire"]}',), ('{"winner": ["Cesta", "Cesta"]}',), ('{"winner": ["Brivenix", "NA", "NA"]}',), ('{"winner": ["Magic4ever_MTGO", "Magic4ever_MTGO"]}',), ('{"winner": ["Pacto_das_Guildas", "Pacto_das_Guildas"]}',), ('{"winner": ["-Sire", "NA", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["CaligulaFanBoi", "CaligulaFanBoi"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Foresterf", "Foresterf"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["NA", "NA", "PauperFeito"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "bergamo83", "-Sire"]}',), ('{"winner": ["ResponsiblyStupid", "ResponsiblyStupid"]}',), ('{"winner": ["lindoso01"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Beicodegeia", "Beicodegeia"]}',), ('{"winner": ["Arsmith8", "-Sire", "Arsmith8"]}',), ('{"winner": ["Corzalan", "Corzalan"]}',), ('{"winner": ["sc00nes", "-Sire", "sc00nes"]}',), ('{"winner": ["-Sire", "cbouz", "NA"]}',), ('{"winner": ["lindoso01", "lindoso01"]}',), ('{"winner": ["-Sire", "callowaysmith1", "-Sire"]}',), ('{"winner": ["Cesta", "Cesta"]}',), ('{"winner": ["Brivenix", "NA", "NA"]}',), ('{"winner": ["Magic4ever_MTGO", "Magic4ever_MTGO"]}',), ('{"winner": ["Pacto_das_Guildas", "Pacto_das_Guildas"]}',), ('{"winner": ["-Sire", "NA", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["CaligulaFanBoi", "CaligulaFanBoi"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Foresterf", "Foresterf"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["NA", "NA", "PauperFeito"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["-Sire", "bergamo83", "-Sire"]}',), ('{"winner": ["ResponsiblyStupid", "ResponsiblyStupid"]}',), ('{"winner": ["lindoso01"]}',), ('{"winner": ["-Sire", "-Sire"]}',), ('{"winner": ["Beicodegeia", "Beicodegeia"]}',), ('{"winner": ["Arsmith8", "-Sire", "Arsmith8"]}',), ('{"winner": ["Hoarse_Duck", "Hoarse_Duck"]}',)]"""
